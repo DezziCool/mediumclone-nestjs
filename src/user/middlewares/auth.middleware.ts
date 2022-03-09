89c9caa95b4375c115e987418eee1855e96b417e
Vladislav <O20.hello-kitty@yandex.ru>
2022-03-09 17:29:29 +0300

    remove Response express + remove comment

--- src/user/middlewares/auth.middleware.ts
+++ src/user/middlewares/auth.middleware.ts
@@ -1,5 +1,5 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { ExpressRequestInterface } from '../types/expressRequest.interfaces';
 import { verify } from 'jsonwebtoken';
 import { JWT_SECRET } from '@app/config';
@@ -11,18 +11,19 @@ export class authMiddleware implements NestMiddleware {
   // Проверяем есть ли у нас токен в headers
   async use(req: ExpressRequestInterface, res: Response, next: NextFunction) {
     if (!req.headers.authorization) {
+      console.log('bad user');
       req.user = null;
       next();
       return;
     }
 
     const token = req.headers.authorization.split(' ')[1]; // вытягиваем токен - он у нас лежит вторым параметром
-    console.log('token', token);
+    // console.log('token', token);
     try {
       const decode = verify(token, JWT_SECRET);
       const user = await this.userService.findById(decode.id);
       req.user = user;
-      console.log(decode);
+      // console.log(decode);
       next();
     } catch (err) {
       req.user = null;
@@ -31,7 +32,7 @@ export class authMiddleware implements NestMiddleware {
   }
 }
 
-// мы можем зарегистрировать middleware на какие то поределенные роуты
+// мы можем зарегистрировать middleware на какие то определенные роуты
 // мы хотим сделать его глобальной для нашего проекта
 // У "Express" изначально нет поля поля "user". Мы могли бы написать "req: any" - но это плохой фикс
 // хороший фикс является расширение интерфейса "Express'a".
@@ -41,3 +42,5 @@ export class authMiddleware implements NestMiddleware {
 // Potential solutions:
 // - If UserService is a provider, is it part of the current AppModule?
 // - If UserService is exported from a separate @Module, is that module imported within AppModule?
+
+// мы не хотим здесь выводить какие либо ошибки, т.к. мы сделали его глобально для всех запросов
