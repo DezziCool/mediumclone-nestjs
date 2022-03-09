836ea390ca43dc545019721856947cf45c7bd645
Vladislav <O20.hello-kitty@yandex.ru>
2022-03-09 14:05:40 +0300

    add midlleware in app.module and made it global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from '@app/ormconfig';
 import { UserModule } from './user/user.module';
+import { authMiddleware } from './user/middlewares/auth.middleware';
 
 @Module({
   imports: [UserModule, TagModule, TypeOrmModule.forRoot(ormconfig)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    // сделали глобальный "middleware"
+    consumer.apply(authMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
 
 // forRoot() - передаем конфиг на вход модуля "TypeOrmModule"
+// когда нам необходимо применить "middleware" - мы описываем ее с помощью конфига
+// и внутри его пишем "consumer.apply"
